e18bb7be87d7d3499067ac4a315ad5430804f329
Algirdas83 <algirdasliutkus@gmail.com>
2022-11-03 13:23:05 +0200

    add modal close funktions for btn and overlai

--- script.js
+++ script.js
@@ -3,16 +3,30 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
-console.log(btnShowModal);
+///Mano variajantas 
+// const closeModal = element => {
+//   element.addEventListener('click', function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   });
+};
 
 for (let i = 0; i < btnShowModal.length; i++) {
   btnShowModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
     //// model closing
-    btnCloseModal.addEventListener('click', function () {
+    /// mano varijantas
+    // closeModal(btnCloseModal);
+    // closeModal(overlay);
+    ///////////////////////////
+    ///Jono variajntas
+    const closeModel2 = function () {
       modal.classList.add('hidden');
       overlay.classList.add('hidden');
-    });
+    };
+
+    btnCloseModal.addEventListener('click', closeModel2);
+    overlay.addEventListener('click', closeModel2);
   });
 }
