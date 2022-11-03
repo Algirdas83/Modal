a283d502cd851fbc6dd6a1c33705978add3964e8
Algirdas83 <algirdasliutkus@gmail.com>
2022-11-03 13:59:15 +0200

    add keydown event for functionality

--- script.js
+++ script.js
@@ -3,30 +3,37 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
-///Mano varijantas 
+///Mano varijantas
 // const closeModal = element => {
 //   element.addEventListener('click', function () {
 //     modal.classList.add('hidden');
 //     overlay.classList.add('hidden');
 //   });
+//};
+//Jono varijantas
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+//Jono varijantas
+const closeModel2 = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    //// model closing
-    /// mano varijantas
-    // closeModal(btnCloseModal);
-    // closeModal(overlay);
-    ///////////////////////////
-    ///Jono varijntas
-    const closeModel2 = function () {
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
-    };
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', openModal);
+//// model closing
+/// mano varijantas
+// closeModal(btnCloseModal);
+// closeModal(overlay);
+///////////////////////////
+///Jono varijntas
+btnCloseModal.addEventListener('click', closeModel2);
 
-    btnCloseModal.addEventListener('click', closeModel2);
-    overlay.addEventListener('click', closeModel2);
-  });
-}
+overlay.addEventListener('click', closeModel2);
+
+document.addEventListener('keydown', function (e) {
+  let key = e.key;
+  if (key === 'Escape') closeModel2();
+});
